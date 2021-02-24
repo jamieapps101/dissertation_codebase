81f087c8376e27cf652cb9278d60c2ac47afb504
jamie <jamieapps101@gmail.com>
2021-02-24 15:14:17 +0000

    Re-designed to match the HAM paper, though the self attention network would be worth investigation

--- persistent_storage/text2topic/app/Neural_Topic_Segmentation/model_generation.py
+++ persistent_storage/text2topic/app/Neural_Topic_Segmentation/model_generation.py
@@ -117,23 +117,31 @@ class custom_SAT(keras.layers.Layer):
         self.reshaper0 = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,LSTM_hidden_state_length))
         self.dense0    = Dense(units=LSTM_hidden_state_length,activation=activations.tanh)
         self.dense1    = Dense(units=LSTM_hidden_state_length,activation=activations.tanh)
-        self.reshaper1 = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,LSTM_hidden_state_length,1))
-        self.reshaper2 = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,1,LSTM_hidden_state_length))
+        self.reshaper1 = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,LSTM_hidden_state_length))
+        self.reshaper2 = Reshape(target_shape=(sentence_sequence_length,LSTM_hidden_state_length,max_sentence_length))
+        self.reshaper3 = Reshape(target_shape=(sentence_sequence_length,LSTM_hidden_state_length))
+        # self.reshaper1 = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,LSTM_hidden_state_length,1))
+        # self.reshaper2 = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,1,LSTM_hidden_state_length))
         self.softmax   = Softmax()
+
+
+        initializer = tf.keras.initializers.RandomUniform(minval=0., maxval=1.)
+
+        self.u_w = tf.Variable(initial_value=initializer((LSTM_hidden_state_length,1,)),
+                               trainable=True)
+
 
 
     def call(self, x):
         temp = self.reshaper0(x)
         keys    = self.dense0(temp)
-        queries = self.dense1(x)
-        # do some weird reshaping here to allow matrix multiplication
         keys    = self.reshaper1(keys)
-        queries = self.reshaper2(queries)
-        x       = linalg.matmul(keys,queries)
+        # queries = self.reshaper2(queries)
+        x       = linalg.matmul(keys,self.u_w)
         x       = self.softmax(x)
-        temp    = self.reshaper1(temp)
-        x       = linalg.matmul(x,temp)
-        x       = self.reshaper0(x)
+        temp    = self.reshaper2(temp)
+        x       = linalg.matmul(temp,x)
+        x       = self.reshaper3(x)
 
         return x
         
@@ -150,7 +158,6 @@ def build_conv_Att_BiLSTM(
     batch_size=1,
     self_attention_enabled=True):
 
-    # inputs = keras.Input(shape=(sentence_sequence_length,max_sentence_length,embedding_length,1,), dtype="float32")
     inputs = keras.Input(shape=(sentence_sequence_length,max_sentence_length,1,embedding_length), batch_size=batch_size,dtype="float32",name="Embedding Input Layer")
     
     x = conv_BiLSTM(
@@ -163,57 +170,35 @@ def build_conv_Att_BiLSTM(
         max_sentence_length,
         LSTM_hidden_state_length,
         embedding_length)(x)
-    
-    # temp = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,LSTM_hidden_state_length))(x)
-
-    # keys = Dense(units=LSTM_hidden_state_length,activation=activations.tanh)(temp)
-    # queries = Dense(units=LSTM_hidden_state_length,activation=activations.tanh)(x)
-    # # do some weird reshaping here to allow matrix multiplication
-    # keys    = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,LSTM_hidden_state_length,1))(keys)
-    # queries = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,1,LSTM_hidden_state_length))(queries)
-    # x=linalg.matmul(keys,queries)
-    # x=Softmax()(x)
-    # temp    = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,LSTM_hidden_state_length,1))(temp)
-    # x=linalg.matmul(x,temp)
-    # x = Reshape(target_shape=(sentence_sequence_length,max_sentence_length,LSTM_hidden_state_length))(x)
-
-    x = custom_SAT(sentence_sequence_length,
+    output = custom_SAT(sentence_sequence_length,
         max_sentence_length,
         LSTM_hidden_state_length,
         embedding_length)(x)
 
-    # else:
-    #     # else use the original paper'ss max pooling
-    #     x = Reshape(target_shape=(max_sentence_length,LSTM_hidden_state_length*2))(x)
-    #     outputs = MaxPooling1D(pool_size=128, strides=None, padding="same", data_format="channels_last")(x)
 
-    model = keras.Model(inputs, x)
-    return model
-    # return inputs, x
+
+    return inputs,output
 
 # This function calls build_Att_BiLSTM to build the global sentence encoder
-def build_sentence_encoder(bert_embedding_length=768):
-    BiLSTM_inputs,BiLSTM_outputs = build_conv_Att_BiLSTM(
-        max_sentence_length = 40, 
-        LSTM_hidden_state_length = 256, 
-        embedding_length = 300,
-        self_attention_enabled=True)
-    
+# output can be "softmax" or "attention"
+def build_sentence_encoder(BiLSTM_outputs,bert_embedding_length=768,output="softmax"):
+
     bert_input = keras.Input(shape=(bert_embedding_length,), dtype="float32")
 
     output = Concatenate()([bert_input,BiLSTM_outputs])
 
-    return [BiLSTM_inputs,bert_input],output
+
+    return bert_input,output
 
 
 
 if __name__=="__main__":
     print("Running from {}".format(__file__))
     
-    model=build_conv_Att_BiLSTM()
+    raw_inputs,outputs=build_conv_Att_BiLSTM()
 
-    # inputs,output = build_sentence_encoder()
-    # model = keras.Model(inputs, output)
+    # bert_inputs,outputs = build_sentence_encoder(outputs)
+    model = keras.Model([raw_inputs], outputs)
     model.summary(line_length=160)
     # plot_model(model,'model.png')
 
