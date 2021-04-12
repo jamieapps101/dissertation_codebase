450bd9d6f9bd8afde8bf3a0b35853e923134480a
jamie <jamieapps101@gmail.com>
2021-04-12 16:02:06 +0100

    started work on deep speech real time processing

--- persistent_storage/speech2text/app/src/app.py
+++ persistent_storage/speech2text/app/src/app.py
@@ -5,11 +5,76 @@ from deepspeech import Model
 import wavSplit
 import webrtcvad
 import time
+import argparse
+import VAD_code
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Deep Speech Application')
+    parser.add_argument('Mode',   metavar='M', nargs='1')
+    parser.add_argument('--File',   metavar='F', nargs='1')
+    parser.add_argument('--Device', metavar='D', nargs='1')
+    args = parser.parse_args()
+
+    # connect to mqtt server
+    
+
+    # load in DS model
+    print("loading model")
+    model_path = os.path.join(os.getcwd(),"models")
+    model = Model(model_path)
+
+    # Start audio with VAD
+    # using default params here
+    vad_audio = VAD_code.VADAudio(
+        aggressiveness=3,# [0,3], 3 filters all non-speech
+        device=args.device,
+        input_rate=16000, # mic sample rate
+        file=None)
+
+    print("Listening (ctrl-C to exit)...")
+    # get iterator to collect frames of audio data
+    frames = vad_audio.vad_collector()
+
+    spinner = None
+    stream_context = model.createStream()
+    wav_data = bytearray()
+    # for each frame
+    for frame in frames:
+        if frame is not None:
+            if spinner: spinner.start()
+            logging.debug("streaming frame")
+            stream_context.feedAudioContent(np.frombuffer(frame, np.int16))
+            # if ARGS.savewav: wav_data.extend(frame)
+        else:
+            if spinner: spinner.stop()
+            # logging.debug("end utterence")
+            # if ARGS.savewav:
+                # vad_audio.write_wav(os.path.join(ARGS.savewav, datetime.now().strftime("savewav_%Y-%m-%d_%H-%M-%S_%f.wav")), wav_data)
+                # wav_data = bytearray()
+            text = stream_context.finishStream()
+            print("Recognized: %s" % text)
+            stream_context = model.createStream()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # This is just a simplified mockup of the deepspeech demo code, 
 # I stole wavSplit file also from the demo code
 
-if __name__ == "__main__":
+if __name__ == "__main__" and False:
     # locate model files
     model_path = os.path.join(os.getcwd(),"models")
     print("model_path: {}".format(model_path))
