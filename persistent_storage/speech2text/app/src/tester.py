10c844b74674a0451cce1b358fae8ec1b9d26716
jamie <jamieapps101@gmail.com>
2021-04-14 10:07:04 +0100

    started work on the testing script for the speech2text module when run in static file transcription mode

---
+++ persistent_storage/speech2text/app/src/tester.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3 -i
+
+# this program is used to test the deep speech audo 
+# input when used in testing mode. it should send an 
+# MQTT message to instruct the module to read a file
+# then varify the text sent back is what the file 
+# actually contained
+
+# files are read in from the testing config json
+# then referenced against the reference data to get
+# the ground truth labels
+
+if __name__=="__main__":
+    print("I need to be populated")
+
+
